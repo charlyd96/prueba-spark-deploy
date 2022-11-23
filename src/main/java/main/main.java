00d9dac6ad4251b696ffeaef9881fc5524b54938
charlyd96 <carlosdesiderio96@gmail.com>
2022-11-22 23:37:46 -0300

    change port

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -4,7 +4,7 @@ import spark.Spark;
 
 public class main {
   public static void main(String[] args) {
-    //Spark.port(8080);
+    Spark.port(8080);
     Spark.get("/ping", (req, resp) -> "pongaaaaa");
   }
 }
