fddee40db394274a78512ad37908ef5664e74364
charlyd96 <carlosdesiderio96@gmail.com>
2022-11-23 00:08:05 -0300

    war

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -1,10 +1,17 @@
 package main;
 
 import spark.Spark;
+import spark.servlet.SparkApplication;
 
-public class main {
+public class main implements SparkApplication {
   public static void main(String[] args) {
-    Spark.port(8081);
+    new main().init();
+  }
+
+  @Override
+  public void init() {
+    //Spark.port(8081);
+    Spark.get("/", (req, resp) -> "home");
     Spark.get("/ping", (req, resp) -> "pongaaaaa");
     Spark.init();
   }
