a838ebd8b5b7ffa7793cb96d4c1ea3b802f3802d
charlyd96 <carlosdesiderio96@gmail.com>
2022-11-22 22:44:27 -0300

    changes

---
+++ src/main/java/main/main.java
@@ -0,0 +1,10 @@
+package main;
+
+import spark.Spark;
+
+public class main {
+  public static void main(String[] args) {
+    //Spark.port(8080);
+    Spark.get("/ping", (req, resp) -> "pongaaaaa");
+  }
+}
