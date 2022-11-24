ac7d0674912af8fc0a21b7360332268e1750dd82
charlyd96 <carlosdesiderio96@gmail.com>
2022-11-24 00:25:57 -0300

    changes

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -4,10 +4,11 @@ import spark.Spark;
 
 public class main {
   public static void main(String[] args) {
-    System.getenv().forEach((k,v) -> System.out.println("KEY: "+ k +  " - VALUE: " + v));
+    //System.getenv().forEach((k,v) -> System.out.println("KEY: "+ k +  " - VALUE: " + v));
     String port = System.getenv("SERVER_PORT");
     Spark.port(Integer.parseInt(port));
     Spark.get("/ping", (req, resp) -> "pongaaaaa");
+    Spark.get("/ponga", (req, resp) -> "ping");
     Spark.init();
   }
 }
