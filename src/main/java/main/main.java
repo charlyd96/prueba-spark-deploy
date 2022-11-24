bd75fb84827279c72cd1f197345c7cd00e6bbe32
charlyd96 <carlosdesiderio96@gmail.com>
2022-11-24 00:31:12 -0300

    cambio de ping

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -7,7 +7,7 @@ public class main {
     //System.getenv().forEach((k,v) -> System.out.println("KEY: "+ k +  " - VALUE: " + v));
     String port = System.getenv("SERVER_PORT");
     Spark.port(Integer.parseInt(port));
-    Spark.get("/ping", (req, resp) -> "pongaaaaa");
+    Spark.get("/ping", (req, resp) -> "Hola estoy respondiendo el ping");
     Spark.get("/ponga", (req, resp) -> "ping");
     Spark.init();
   }
